5cd86198718092a7bab640356eea22ea00ee15bd
santosandressa <andressasantosp0@gmail.com>
2023-07-11 15:36:54 -0300

    workflow github action

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+
+describe('AppComponent', () => {
+  beforeEach((() => {
+    TestBed.configureTestingModule({
+      imports: []
+
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+});
